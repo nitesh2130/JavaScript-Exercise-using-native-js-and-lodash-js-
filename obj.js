f896a11e71233e59969b0cd9a7c928e20f886c47
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-01-20 10:22:37 +0530

    Initial commit

---
+++ obj.js
@@ -0,0 +1,9 @@
+const members = [
+  {name: 'Aman Makhija', age: 20},
+  {name: 'Suresh Jsharma', age: 40},
+  {name: 'Vijay Sahu', age: 41},
+  {name: 'Rakesh Srivastava', age: 17},
+  {name: 'Chandraprakash Sharma'},
+  {name: 'Swpril Ahuja', age: 45},
+  {name: 'Yogesh Khatri', age: 51}
+];
